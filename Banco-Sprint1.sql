b5cf007e2c02825d23058facd3e06ef15b086fcf
KennedyMartinsB <79326291+KennedyMartinsB@users.noreply.github.com>
2021-03-11 12:23:00 -0300

    Banco de dados para a Sprint1
    Banco atualizado

--- Banco-Sprint1.sql
+++ Banco-Sprint1.sql
@@ -12,34 +12,44 @@ create table cliente(
 
 create table carne(
 	idCarne int primary key auto_increment,
+    fornecedor varchar(50),
 	marca varchar(50),
 	tipo varchar(50)
 );
 
 create table sensor(
 	idSensor int primary key auto_increment,
-	temperaturaMedia int
+    nome varchar(50),
+    localizacaoSensor varchar(50),
+	temperatura float not null,
+    dataTemperatura datetime
 );
 
 create table freezer(
 	idFreezer int primary key auto_increment,
-    capacidadeFreezer int
+    nome varchar(50),
+    capacidade double
 );
 
 create table estabelecimento(
 	idEstabelecimento int primary key auto_increment,
-    qtdRefrigeradores int
+    nomeEstabelecimento varchar(50),
+    endereco varchar(150),
+    cnpj char(20)
 );
 
 create table central(
 	idCentral int primary key auto_increment,
+    enderecoCentral varchar(150),
 	msgMediaTemperatura varchar(150),
     msgFalhaRefrigeracao varchar(150)
 );
 
 INSERT INTO cliente (nome, estado, endereco, cnpj, mediaDescarte) VALUES 
 					('Friboi', 'São Paulo','Av. Marginal Direita do Tietê, 500 - Vila Jaguara, São Paulo', '02.916.265/0001-60', '15%'),
-                    ('Swift', 'São Paulo', ' R. Cristóvão de Gouvêia, 320 - Parque Anhanguera, São Paulo ', '36.425.014/0001-02', '12%'),
+                    ('Swift', 'São Paulo', 'R. Cristóvão de Gouvêia, 320 - Parque Anhanguera, São Paulo ', '36.425.014/0001-02', '12%'),
+                    ('Gold Meat', 'São Paulo', 'Estr. Velha de Itu, 654-982 - Jardim Alvorada, Jandira - SP', '01.543.135/0001-34', '10%'),
+                    ('Minerva', 'São Paulo', 'Rua Sebastião Bach Nº307 4ºANDAR - vila leopoldina, São Paulo','	67.620.377/0001-14', '13%'),
                     ('Montana', 'São Paulo', 'R. Acarape, 559 - Jardim Cambui, Santo André','19.490.389/0001-29', '20%');
                     
 -- Exibir todos os campos da tabela cliente                    
@@ -64,7 +74,7 @@ SELECT * FROM cliente WHERE nome = 'Montana';
 SELECT * FROM cliente WHERE nome LIKE '%S%';
 
 -- Exibir nome do cliente terminado com determinada letra
-SELECT * FROM cliente WHERE nome LIKE '%i';
+SELECT * FROM cliente WHERE nome LIKE '%t';
 
 -- Exibir nome do cliente com determinada letra como penultima
 SELECT * FROM cliente WHERE nome LIKE '%n_';
